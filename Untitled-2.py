96788c513c69151625b7870cbaac51160d3a86ac
akarsh330 <akarshgowda7711@gmail.com>
2024-12-17 09:06:51 +0530

    Create Untitled-2.py

---
+++ Untitled-2.py
@@ -0,0 +1,15 @@
+while True:
+    print('1. payment failed')
+    print('2. profile and privacy')
+    print('3. money tyransfer')
+    print('4. exit')
+    user_inp=int(input('enter ur choice'))
+    if user_inp==1:
+        print('wellcome to payment failed')
+    elif user_inp==2:
+        print('wellcome to profile and privacy')
+    elif user_inp==3:
+        print('wellcome to money transfer')
+    elif user_inp==4:
+        print('exit')
+        break
