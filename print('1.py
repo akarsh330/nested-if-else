837dc390bfc0cf51239c33421b7ac9ae6c643159
akarsh330 <akarshgowda7711@gmail.com>
2024-12-27 15:24:49 +0530

    Create print('1.py

---
+++ print('1.py
@@ -0,0 +1,10 @@
+print('1. payment issues')
+print('2. profile and payments')
+print('3. money transfer')
+print('4. recharge and bill payments')
+print('5. exit')
+user_input = input('enter your choice')
+if user_input==1:
+    print('welcome to payment issues')
+    elif
+    
